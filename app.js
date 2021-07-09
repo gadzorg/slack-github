b20a92adbd386ba011b2e8bc2c0cca9993d71596
Colin Darie <colin@darie.eu>
2021-07-09 13:17:00 +0200

    Update for Express 4.x, supports new Github API

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 /* required dependencies */
-var request = require('request');
-var express = require('express');
+var request = require("request");
+var express = require("express");
+var bodyParser = require("body-parser");
 
 /* app instance */
 var app = express();
 
 /* app configurations */
-app.use(express.logger());
-app.use(express.bodyParser());
+// parse application/json
+app.use(bodyParser.json());
 
 /* redirects to GitHub Repo of the module */
-app.get('/', function(req, res) {
-  res.redirect('https://github.com/sdslabs/slack-github');
+app.get("/", function (_req, res) {
+  res.redirect("https://github.com/gadzorg/slack-github");
 });
 
 /* config variables */
@@ -20,60 +21,74 @@ var username = process.env.USERNAME;
 var url = process.env.URL;
 
 /* returns genarated message to send using request payload by GitHub */
-var generateMessage = function(req) {
-  var result = '';
+var generateMessage = function (req) {
+  var result = "";
 
   var data = req.body;
 
-  for(var i=0;i<data.commits.length;i++)
-  {
-    var commit = data.commits[i];
+  for (var i = 0; i < data.commits.length; i++) {
+    var { commit } = data.commits[i];
     var repo = data.repository;
 
-    result += '<@' + commit.author.username + '>' + ' <' +commit.url+  '|committed:> in <' + repo.url + '|' + repo.name + '> :' + commit.message;
-    result += '\n';
+    result +=
+      "<@" +
+      commit.author.name +
+      ">" +
+      " <" +
+      commit.url +
+      "|committed> in <" +
+      repo.url +
+      "|" +
+      repo.name +
+      "> : " +
+      commit.message;
+    result += "\n";
   }
 
   return result;
-}
+};
 
 /*
 triggers on a POST request by GitHub webhook
 and send message to slack-channel, according to commit detail
 */
-app.post('/', function(req, res) {
-
+app.post("/", function (req, res) {
   /* works only, if url config var is there */
-  if(url)
-  {
-    console.log("Responding to event '%s'", req.headers['x-github-event'])
-    if (req.headers['x-github-event'] === 'ping') {
-      return res.status(200).json({value: 'pong'});
+  if (url) {
+    console.log("Responding to event '%s'", req.headers["x-github-event"]);
+    if (req.headers["x-github-event"] === "ping") {
+      return res.json({ value: "pong" });
+    }
+
+    if (req.headers["x-github-event"] !== "push") {
+      return res.sendStatus(204);
     }
 
     var options = {};
     options.url = url;
-    options.method = 'POST';
+    options.method = "POST";
 
     /* use default channel and username if they are not present in config */
     options.body = {};
-    if(channel) {options.body['channel'] = '#'+ channel +'';}
-    if(username) {options.body['username'] = ''+ username +'';}
+    if (channel) {
+      options.body["channel"] = "#" + channel;
+    }
+    if (username) {
+      options.body["username"] = "" + username;
+    }
     console.log("Would be sending message '%s'", generateMessage(req));
-    options.body['text'] = generateMessage(req);
+    options.body["text"] = generateMessage(req);
 
     options.json = true;
 
-    request(options, function (err, response, body) {
-      var headers = response.headers;
+    request(options, function (_err, response, _body) {
       var statusCode = response.statusCode;
-      res.send(statusCode);
+      res.sendStatus(statusCode);
     });
   }
-
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("express server listening on " + port);
 });
